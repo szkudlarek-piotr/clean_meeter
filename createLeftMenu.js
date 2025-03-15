3f6a50840981ee413ff76140aa13badd9fd9cb08
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2025-03-15 23:28:22 +0100

    Removed one unnecessery option from the left menu and changed names of two others.

--- createLeftMenu.js
+++ createLeftMenu.js
@@ -2,12 +2,11 @@ async function createLeftMenu() {
     let leftMenu = document.getElementsByClassName("left-menu")[0]
     const menuButtonsArray = [{"id":"mainPage","text": "Strona główna", "href": "index.html"}, 
         {"id":"addNewHuman","href": "add-human.html", "text": "Dodaj nowego człowieka"},
-        {"id": "calendar","href": "new_calendar.html", "text": "Zobacz kalendarz"},
+        {"id": "calendar","href": "calendar.html", "text": "Zobacz kalendarz"},
          {"id": "showAllCliques","href": "all-cliques.html", "text": "Pokaż wszystkie kliki"},
           {"id": "showAllPeople","href": "final-humans.html", "text": "Zobacz wszystkich ludzi"}, 
-          {"id": "addNewMeeting", "href": "add_meeting.html", "text": "Dodaj nowe spotanie"}, 
+          {"id": "addNewMeeting", "href": "add-meeting.html", "text": "Dodaj nowe spotanie"}, 
           {"id": "addNewClique", "href": "add-clique.html", "text": "Dodaj nową klikę"}, 
-          {"id": "addHumansToMeeting", "text": "Dodaj ludzi do spotkania", "href": "add_humans_to_meeting.html"},
            {"id": "addVisit", "href": "add-visit.html", "text": "Dodaj wizytę!"}, 
            {"id": "addQuote", "href": "add-quote.html", "text": "Dodaj Złoty Cytat"}, 
            {"id": "addEvent", "href": "add-event.html", "text": "Dodaj wydarzenie"}, {"id": "guessQuotes", "href": "guess-quotes.html", "text": "Odgadnij autora cytatu!"}]
