bbe92bc4156e4140e392c67baf229a0820b3dbb2
Piotr Szkudlarek <pgszkudlarek@gmail.com>
2025-07-20 22:13:12 +0200

    Craeted a function that checks if thare are any points on a map in a human modal to show.

---
+++ backend/getDefinedPlacesNumber.js
@@ -0,0 +1,65 @@
+import dotenv from 'dotenv'
+import mysql from 'mysql2'
+dotenv.config()
+const pool = mysql.createPool({
+    host     : process.env.host,
+    user     : process.env.MYSQL_USER,
+    database : process.env.MYSQL_DATABASE
+}).promise()
+
+export default async function getDefinedPlacesNumber(humanId) {
+    const queryText = `WITH all_interactions AS (SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM visits
+    JOIN places ON visits.place_id = places.id
+    WHERE visits.visit_id IN (SELECT visit_guest.visit_id FROM visit_guest WHERE visit_guest.guest_id = ?)
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM meetings
+    JOIN places ON meetings.place_id = places.id
+    WHERE meetings.ID IN (SELECT meeting_human.meeting_id FROM meeting_human WHERE meeting_human.human_id = ?)
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM events
+    JOIN places ON events.place_id = places.id
+    WHERE events.id IN (SELECT event_companion.event_id FROM event_companion WHERE event_companion.human_id = ?)
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM weddings
+    JOIN places ON places.id = weddings.wedding_place_id
+    WHERE weddings.man_id = ? OR weddings.woman_id = ? OR weddings.partner_id = ?
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM weddings
+    JOIN places ON places.id = weddings.party_place_id
+    WHERE weddings.man_id = ? OR weddings.woman_id = ? OR weddings.partner_id = ?
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM weddings
+    JOIN places ON places.id = weddings.wedding_place_id
+    WHERE weddings.id IN (SELECT wedding_guest.wedding_id FROM wedding_guest WHERE wedding_guest.guest_id = ?)
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM weddings
+    JOIN places ON places.id = weddings.party_place_id
+    WHERE weddings.id IN (SELECT wedding_guest.wedding_id FROM wedding_guest WHERE wedding_guest.guest_id = ?)
+    group by places.id
+    UNION
+    SELECT places.place_name, places.category, places.latitude AS lat, places.longitude AS lng, COUNT(places.id) as place_count
+    FROM citybreak_companion
+    JOIN citybreaks ON citybreaks.ID = citybreak_companion.citybreak_id
+    JOIN trip_place ON trip_place.trip_id = citybreaks.ID
+    JOIN places ON places.id = trip_place.place_id
+    WHERE citybreak_companion.human_id = ?
+    GROUP BY places.id)
+    SELECT COUNT(*) AS places_number FROM all_interactions
+    `
+    const [queryResult] = await pool.query(queryText, [humanId, humanId, humanId, humanId, humanId, humanId, humanId, humanId, humanId, humanId, humanId, humanId])
+    const returnedValue = queryResult[0]["places_number"]
+    return returnedValue
+}
